a83d766e4299282917272473c43372da8f79a066
AbdulWahid100 <105293012+AbdulWahid100@users.noreply.github.com>
2023-01-31 18:49:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,12 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+# Create an EC2 instance
+resource "aws_instance" "hello-world" {
+  ami           = "ami-06c39ed6b42908a36"
+  instance_type = "t2.micro"
+  subnet_id="subnet-0b2cc8cfd58e5c754"
+    tags = {
+    Name = "HelloWorld"
+  }
 }
