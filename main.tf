0ca67f92e5eaafdeea2da76b9f560f84883dcba9
AbdulWahid100 <105293012+AbdulWahid100@users.noreply.github.com>
2023-01-31 18:25:10 +0100

    create new file
    create new file

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "eu-central-1"
+  default_tags {
+    tags = {
+      "creator" : "terraform",
+      "environment" : "integration",
+      "product-name" : "Terraform Enterprise - TFE",
+      "source-repo" : "https://git-codecommit.eu-central-1.amazonaws.com/v1/repos/terraform-infra-tfe",
+    }
+  }
+//  profile = "798859060341"
+
+}
+
+# Create an EC2 instance
+resource "aws_instance" "hello-world" {
+  ami           = "ami-06c39ed6b42908a36"
+  instance_type = "t2.micro"
+  subnet_id="subnet-0b2cc8cfd58e5c754"
+    tags = {
+    Name = "HelloWorld"
+  }
+}
