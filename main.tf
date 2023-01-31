ea902f127575b4e90d6001c236e57f7d71c5515f
AbdulWahid100 <105293012+AbdulWahid100@users.noreply.github.com>
2023-01-31 18:45:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,16 @@ resource "aws_instance" "hello-world" {
     Name = "HelloWorld"
   }
 }
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
