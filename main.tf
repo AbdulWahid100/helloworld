0f9eb04a53b731eac5e698150bc5c43d1036152e
AbdulWahid100 <105293012+AbdulWahid100@users.noreply.github.com>
2023-02-01 16:21:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ resource "aws_instance" "hello-world" {
     Name = "HelloWorld"
   }
 }
+# Create an EC2 instance
+resource "aws_instance" "hello-world-another" {
+  ami           = "ami-06c39ed6b42908a36"
+  instance_type = "t2.micro"
+  subnet_id="subnet-0b2cc8cfd58e5c754"
+    tags = {
+    Name = "HelloWorldAnoter"
+  }
+}
